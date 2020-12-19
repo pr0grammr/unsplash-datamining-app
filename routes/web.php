913e1d09358027345b48b7647583cfab93c48084
Fabian Schilf <fabian.schilf@active-value.de>
2020-12-20 00:16:31 +0100

    define routes

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/unsplash', [UnsplashController::class, 'show'])->name('unsplash-index');
 Route::get('/unsplash/users/{unsplashUser}', [UnsplashController::class, 'showUserDetail'])->name('unsplash-user-detail');
+Route::get('/unsplash/photos/{unsplashPhoto}', [UnsplashController::class, 'showPhotoDetail'])->name('unsplash-photo-detail');
 Route::post('/unsplash/analyze', [UnsplashController::class, 'analyzeInput'])->name('unsplash-analyze');
