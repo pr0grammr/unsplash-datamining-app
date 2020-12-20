448472d42941c40fe5159e51445de7e9d1e28874
Fabian Schilf <fabian.schilf@active-value.de>
2020-12-20 23:28:43 +0100

    calculate likes from user photos

--- app/Unsplash/UserService.php
+++ app/Unsplash/UserService.php
@@ -53,13 +53,36 @@ class UserService
         $data = $user->toArray();
 
         $userStats = $user->statistics();
-        $totalLikes = $userStats->offsetGet('likes')->total;
         $totalViews = $userStats->offsetGet('views')->total;
+        $totalLikes = $this->calculateLikesFromPhotos($user);
 
         $data['total_views'] = $totalViews;
         $data['total_likes'] = $totalLikes;
         $data['profile_image_url'] = $data['profile_image']['medium'];
 
         return $data;
+    }
+
+    /**
+     * die unsplash API bietet keine möglichkeit die gesamten likes eines users
+     * zu bekommen. daher müssen hier die likes von jedem foto zusammengerechnet werden
+     * das limit wird auf 50 gesetzt um weniger API requests machen zu müssen
+     *
+     * @param User $user
+     * @return float|int
+     */
+    private function calculateLikesFromPhotos(User $user)
+    {
+        $limit = 50;
+        $pages = ceil($user->total_photos / $limit);
+        $likes = [];
+
+        for ($i = 1; $i <= $pages; $i++) {
+            foreach ($user->photos($i, $limit) as $photo) {
+                $likes[] = $photo->likes;
+            }
+        }
+
+        return array_sum($likes);
     }
 }
